ec76508831cf30a92ee7297b635ad972abdf1fd1
Daniel Krancz <daniel.krancz@clouddna.at>
2023-09-21 05:46:04 +0000

    Example 4 Lokal-JSON

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -9,7 +9,14 @@ sap.ui.define([
 
         return Controller.extend("at.clouddna.training00.zhoui5.controller.Main", {
             onInit: function () {
-
+                
+            },
+            genderFormatter: function(sKey){
+                let oView = this.getView();
+                let oI18nModel = oView.getModel("i18n");
+                let oResourceBundle = oI18nModel.getResourceBundle();
+                let sText = oResourceBundle.getText(sKey);
+                return sText;
             }
         });
     });
